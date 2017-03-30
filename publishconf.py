47cd51168f25127ed4a8a4294105ab061c36e6b1
Peijun Zhu <zpj.ustc@gmail.com>
2017-03-30 13:29:14 -0400

    add ipynb as submodule

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://peijunz.github.io'
+SITEURL = 'https://peijunz.github.io'
 RELATIVE_URLS = False
 USE_LESS = False
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
